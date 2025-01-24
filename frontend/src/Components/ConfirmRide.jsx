c41cab96167662a828780022dd2d5f40ee00e938
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2025-01-25 00:38:51 +0530

    Update ConfirmRide.jsx

--- frontend/src/Components/ConfirmRide.jsx
+++ frontend/src/Components/ConfirmRide.jsx
@@ -14,14 +14,14 @@ const ConfirmRide = (props) => {
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="ri-map-pin-user-fill"></i>
                         <div>
-                            <h3 className='text-lg font-medium'>Pick-up</h3>
+{/*                             <h3 className='text-lg font-medium'>Pick-up</h3> */}
                             <p className='text-sm -mt-1 text-gray-600'>{props.pickup}</p>
                         </div>
                     </div>
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="text-lg ri-map-pin-2-fill"></i>
                         <div>
-                            <h3 className='text-lg font-medium'>Destination</h3>
+{/*                             <h3 className='text-lg font-medium'>Destination</h3> */}
                             <p className='text-sm -mt-1 text-gray-600'>{props.destination}</p>
                         </div>
                     </div>
@@ -29,16 +29,16 @@ const ConfirmRide = (props) => {
                         <i className="ri-currency-line"></i>
                         <div>
                             <h3 className='text-lg font-medium'>₹{props.fare[ props.vehicleType ]}</h3>
-                            <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
+{/*                             <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p> */}
                         </div>
                     </div>
-                </div>
                 <button onClick={() => {
                     props.setVehicleFound(true)
                     props.setConfirmRidePanel(false)
                     props.createRide()
 
                 }} className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
+                </div>
             </div>
         </div>
     )
